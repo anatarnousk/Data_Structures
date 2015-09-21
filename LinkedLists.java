b25eab724cc216a3c8436b5b158db60cf7ddbf76
Anastasiya Tarnouskaya <12at59@queensu.ca>
2015-09-20 21:52:58 -0400

    Added adding elements to a linked list

--- LinkedLists.java
+++ LinkedLists.java
@@ -1,3 +1,9 @@
+/*
+ *	Java Implementation of Linked List Data Structure
+ *	@author: Anastasiya Tarnouskaya
+ *
+ */
+
 /* Linked list class */
 
 public class Linked_List{
@@ -5,7 +11,7 @@ public class Linked_List{
 
 	//constructor for an empty Linked List
 	public Linked_List(){
-		head = new Node(null);
+		head = null;
 	}
 
 	//constructor for non-empty Linked List
@@ -13,7 +19,50 @@ public class Linked_List{
 		head = node;
 	}
 
+	//adds to the beginning of a linked list - O(1)
+	public void add(Object data){
+		if (head == null){
+			head = new Node(data);
+		}
+		else{
+			head = new Node(data, head);
+		}
+	}
 
+	//adds to the end of a linked list - 0(n)
+	public void addToEnd(Object data){
+		if (head == null){
+			head = new Node(data);
+		}
+		else{
+			current = head
+			while (head.getNext() != null){
+				current = head.next;
+			}
+			current.next = new Node(data);
+		}
+	}
+
+	//adds to linked list to a particular index - O(n)
+	public void addToIndex(Object data, int index){
+		if (index < 0){
+			return;
+		}
+		int count = 0;
+		Node current = head;
+		while (count < (index - 1)){
+			if (current.next == null){
+				return;
+			}
+			else{
+				current = current.next;
+			}
+			count ++;
+		}
+		tempTail = current.next;
+		current.data = new Node(data);
+		current.next = tempTail;
+	}
 
 }
 
@@ -34,5 +83,9 @@ public class Node{
 	public Node(Object data, Node next){
 		this.data = data;
 		this.next = next;
+	}
+
+	public Node getNext(Node head){
+		return head.next;
 	}
 }
