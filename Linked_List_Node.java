6fee4410563db66636cd24ac29bd32d018f7e8e4
Anastasiya Tarnouskaya <12at59@queensu.ca>
2015-09-21 02:08:59 -0400

    refactoring

--- Linked_List_Node.java
+++ Linked_List_Node.java
@@ -6,23 +6,23 @@
 
 /* Node class */
 
-public class Node{
+public class Linked_List_Node{
     Object data;
-    Node next;
+    Linked_List_Node next;
 
     //constructor for a node with no next value
-    public Node(Object data){
+    public Linked_List_Node(Object data){
         this.data = data;
         next = null;
     }
 
     //constructor for node with specified next value
-    public Node(Object data, Node next){
+    public Linked_List_Node(Object data, Linked_List_Node next){
         this.data = data;
         this.next = next;
     }
 
-    public Node getNext(){
+    public Linked_List_Node getNext(){
         return next;
     }
 }
